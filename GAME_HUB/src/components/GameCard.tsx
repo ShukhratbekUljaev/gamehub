10f8302fd4b729c3192d1fa9163d1c24c2c12772
Shukhratbek Uljaev <eyehorus42@gmail.com>
2023-07-26 14:52:25 -0700

    get optimized images

--- GAME_HUB/src/components/GameCard.tsx
+++ GAME_HUB/src/components/GameCard.tsx
@@ -2,15 +2,22 @@ import { Game } from "../hooks/useGames";
 import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
+import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
   game: Game;
 }
 
-const GameCard = ({ game }: Props) => {
+/**
+ * Renders a card component that displays information about a game.
+ *
+ * @param {Object} game - An object of type Game that contains information about the game to be displayed.
+ * @returns {JSX.Element} A Card component that displays the game's name, background image, parent platforms, and critic score.
+ */
+const GameCard = ({ game }: Props): JSX.Element => {
   return (
     <Card borderRadius={10} overflow="hidden">
-      <Image src={game.background_image} />
+      <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
         <HStack justifyContent="space-between">
