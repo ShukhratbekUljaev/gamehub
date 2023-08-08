e0ae29ab56057cbfa754d86fc65514fc9c2adc8c
Shukhratbek Uljaev <eyehorus42@gmail.com>
2023-08-07 18:54:16 -0700

    Refactor extract query object

--- GAME_HUB/src/hooks/useGames.ts
+++ GAME_HUB/src/hooks/useGames.ts
@@ -1,5 +1,5 @@
+import { GameQuery } from "../App";
 import useData from "./useData";
-import { Genre } from "./useGenres";
 
 
 
@@ -22,7 +22,7 @@ export interface Game {
   
  
 
-const useGames = (selectedGenre: Genre | null, selectedPlatform: Platform | null) => useData<Game>('/games', {params: {genres: selectedGenre?.id, platforms: selectedPlatform?.id}}, [selectedGenre?.id, selectedPlatform?.id]);
+const useGames = (gameQuery: GameQuery) => useData<Game>('/games', {params: {genres: gameQuery.genre?.id, platforms: gameQuery.platform?.id}}, [gameQuery]);
 
 
 export default useGames;
